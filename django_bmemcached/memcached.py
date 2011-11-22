e813d69ff6fd0b00dd4c002520e11d090a05dff7
Jayson Reis <santosdosreis@gmail.com>
2011-11-22 14:02:13 -0200

    added inicial features

--- django_bmemcached/memcached.py
+++ django_bmemcached/memcached.py
@@ -2,4 +2,29 @@ from django.core.cache.backends import memcached
 
 
 class BMemcached(memcached.BaseMemcachedCache):
-    pass
+    """
+    An implementation of a cache binding using python-binary-memcached
+    A.K.A BMemcached.
+    """
+    def __init__(self, server, params):
+        import bmemcached
+        super(BMemcached, self).__init__(server, params,
+             library=bmemcached,
+             value_not_found_exception=ValueError)
+
+    @property
+    def _cache(self):
+        client = getattr(self, '_client', None)
+        if client:
+            return client
+
+        if self._options:
+            client = self._lib.Client(self._servers,
+                self._options.get('username', None),
+                self._options.get('password', None))
+        else:
+            client = self._lib.Client(self._servers)
+
+        self.client = client
+
+        return client
--- tests/__init__.py
+++ tests/__init__.py
@@ -5,7 +5,8 @@ import django_bmemcached
 
 class Tests(unittest.TestCase):
     def setUp(self):
-        self.client = django_bmemcached.BMemcached()
+        self.client = django_bmemcached.BMemcached(('127.0.0.1:11211', ),
+            {'OPTIONS': {'username': 'user', 'password': 'password'}})
 
     def testGet(self):
         self.assertEqual('value', self.client.get('key'))
