cf0262a2cbf7d0a793c98cc87de74d4bfdcaf9db
Jayson Reis <santosdosreis@gmail.com>
2011-11-22 14:06:33 -0200

    added some tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -8,5 +8,15 @@ class Tests(unittest.TestCase):
         self.client = django_bmemcached.BMemcached(('127.0.0.1:11211', ),
             {'OPTIONS': {'username': 'user', 'password': 'password'}})
 
+    def tearDown(self):
+        self.client.delete('key')
+
     def testGet(self):
+        self.client.set('key', 'value')
         self.assertEqual('value', self.client.get('key'))
+
+    def testDelete(self):
+        self.client.set('key', 'value')
+        self.assertEqual('value', self.client.get('key'))
+        self.client.delete('key')
+        self.assertEqual(None, self.client.get('key'))
