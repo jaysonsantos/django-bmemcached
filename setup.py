66de54248d38242a63d8ac530eb0dee6319fe550
Jayson Reis <jayson.reis@sabbre.com.br>
2011-12-15 10:56:37 -0200

    added setup.py, why i forgot? nvm

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+setup(
+    name='django-bmemcached',
+    version='0.2',
+    author='Jayson Reis',
+    author_email='santosdosreis@gmail.com',
+    description='A Django cache backend to use bmemcached module which ' + \
+        'supports memcached binary protocol with authentication.',
+    url='https://github.com/jaysonsantos/django-bmemcached',
+    packages=['django_bmemcached'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
