969cf726d8dfe621e1a7fb3caa566ff1a0fa39ad
Jayson Reis <santosdosreis@gmail.com>
2020-01-09 13:46:55 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-bmemcached',
-    version='0.2.4',
+    version='0.3.0',
     author='Jayson Reis',
     author_email='santosdosreis@gmail.com',
     description='A Django cache backend to use bmemcached module which ' +
@@ -16,6 +16,11 @@ setup(
         'Framework :: Django :: 1.8',
         'Framework :: Django :: 1.9',
         'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
@@ -26,5 +31,6 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ]
 )
